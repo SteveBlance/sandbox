e21e52f5fa49c99df12c640791d26f11c0cb93cb
SteveBlance <stephen.blance@gmail.com>
2015-03-07 00:41:51 +0000

    Just something to start it off. Example of static import.

---
+++ src/com/codaconsultancy/sandbox/ocpjp/StaticImport.java
@@ -0,0 +1,13 @@
+package com.codaconsultancy.sandbox.ocpjp;
+
+import static java.lang.System.out;
+import static java.math.BigDecimal.valueOf;
+
+public class StaticImport {
+
+    public static void main(String[] args) {
+        out.println("Hello");
+        out.println("World");
+        out.println(valueOf(2L));
+    }
+}
